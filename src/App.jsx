d308b10c86dace92566db0af606b74247ceb8bfa
amanmishra75way <amishra.75way@gmail.com>
2025-06-20 16:54:34 +0530

    Add i18n support with i18next and language detection
    - Updated package.json and package-lock.json to include i18next, i18next-browser-languagedetector, and react-i18next dependencies.
    - Enhanced App component to utilize i18next for internationalization.
    - Created i18n.js for i18next initialization and language resource management.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="flex flex-col h-screen w-full bg-black text-white p-10 justify-center items-center">
-      <h1>Welcome to My React App</h1>
+      <h1 className="text-4xl">Welcome to My React App</h1>
     </div>
   );
 };
---
+++ src/i18n.js
@@ -0,0 +1,23 @@
+import i18next, { init } from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    lng: "en",
+    resources: {
+      en: {
+        translation: {
+          greeting: "Welcome to My React App",
+        },
+      },
+      hi: {
+        translation: {
+          greeting: "मेरे रिएक्ट ऐप में आपका स्वागत है",
+        },
+      },
+    },
+  });
